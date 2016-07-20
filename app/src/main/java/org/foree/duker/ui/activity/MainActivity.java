303abfa950525144a9902478914c3f3fa7f329ae
foree <beijing.2008.lm@163.com>
2016-07-20 21:29:00 +0800

    添加简单的Drawlayou实现

--- app/src/main/java/org/foree/duker/ui/activity/MainActivity.java
+++ app/src/main/java/org/foree/duker/ui/activity/MainActivity.java
@@ -1,19 +1,23 @@
 package org.foree.duker.ui.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.ExpandableDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import org.foree.duker.R;
 import org.foree.duker.api.AbsApiFactory;
@@ -21,12 +25,12 @@ import org.foree.duker.api.AbsApiHelper;
 import org.foree.duker.api.ApiFactory;
 import org.foree.duker.api.FeedlyApiHelper;
 import org.foree.duker.base.BaseActivity;
-import org.foree.duker.net.NetCallback;
-import org.foree.duker.rssinfo.RssCategory;
-
-import java.util.List;
 
 public class MainActivity extends BaseActivity{
+    private static final int PROFILE_SETTING = 100000;
+
+    private AccountHeader headerResult = null;
+    private Drawer result = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +39,51 @@ public class MainActivity extends BaseActivity{
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Create a few sample profile
+        // NOTE you have to define the loader logic too. See the CustomApplication for more details
+        final IProfile profile = new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon("https://avatars3.githubusercontent.com/u/1476232?v=3&s=460").withIdentifier(100);
 
-        new DrawerBuilder().withActivity(this).build();
+        // Create the AccountHeader
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(true)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        profile,
+                        //don't ask but google uses 14dp for the add account icon in gmail but 20dp for the normal icons (like manage account)
+                        new ProfileSettingDrawerItem().withName("Add Account").withDescription("Add new GitHub Account").withIdentifier(PROFILE_SETTING),
+                        new ProfileSettingDrawerItem().withName("Manage Account").withIdentifier(100001)
+                )
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withHasStableIds(true)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        new ExpandableDrawerItem().withName("Photo").withIdentifier(1).withSelectable(false).withSubItems(
+                                new SecondaryDrawerItem().withName("xxxx1").withLevel(2).withIdentifier(2000),
+                                new SecondaryDrawerItem().withName("xxxx2").withLevel(2).withIdentifier(2001)
+                        )
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null){
+                            Intent intent = null;
+                            if (drawerItem.getIdentifier() == 1){
+                                Toast.makeText(getApplicationContext(),"1",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        return false;
+                    }
+
+                })
+                .withSavedInstance(savedInstanceState)
+                .withShowDrawerOnFirstLaunch(true)
+                .build();
 
         AbsApiFactory absApiFactory = new ApiFactory();
         final AbsApiHelper apiHelper = absApiFactory.createApiHelper(FeedlyApiHelper.class);
