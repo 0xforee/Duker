0754e323b240b55ec6f65d8357ed68b3c22709d5
foree <beijing.2008.lm@163.com>
2016-07-22 21:44:03 +0800

    在app内部load Url而不是使用浏览器load

--- app/src/main/java/org/foree/duker/ui/activity/ArticleActivity.java
+++ app/src/main/java/org/foree/duker/ui/activity/ArticleActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import org.foree.duker.R;
 import org.foree.duker.base.BaseActivity;
@@ -22,7 +23,13 @@ public class ArticleActivity extends BaseActivity {
         wb_article = (WebView)findViewById(R.id.wb_article);
         String Url = getIntent().getStringExtra("entryUrl");
         Log.d(TAG, Url);
-
+        wb_article.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
         wb_article.loadUrl(Url);
 
     }
