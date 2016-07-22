16ae1fd6f2eb00996eaf0cdff6b704249a2af22e
foree <beijing.2008.lm@163.com>
2016-07-22 19:47:27 +0800

    添加RecyclerView

--- app/src/main/java/org/foree/duker/ui/fragment/ItemListFragment.java
+++ app/src/main/java/org/foree/duker/ui/fragment/ItemListFragment.java
@@ -3,6 +3,8 @@ package org.foree.duker.ui.fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,17 +22,19 @@ public class ItemListFragment extends Fragment{
     private static final String KEY_TITLE = "title";
 
     private Drawer result;
+    private RecyclerView mRecyclerView;
+    private ItemListAdapter mAdapter;
 
     public ItemListFragment() {
         // Required empty public constructor
     }
 
-    public static ItemListFragment newInstance(String title) {
+    public static ItemListFragment newInstance(String feedId) {
         ItemListFragment f = new ItemListFragment();
 
         Bundle args = new Bundle();
 
-        args.putString(KEY_TITLE, title);
+        args.putString(KEY_TITLE, feedId);
         f.setArguments(args);
 
         return (f);
@@ -44,6 +48,39 @@ public class ItemListFragment extends Fragment{
         TextView tv = (TextView)linearLayout.findViewById(R.id.tv_test_feed_id);
         tv.setText(getArguments().getString(KEY_TITLE));
 
+        mRecyclerView = (RecyclerView) linearLayout.findViewById(R.id.rv_item_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mRecyclerView.setAdapter(mAdapter = new ItemListAdapter());
+
         return linearLayout;
+    }
+
+    class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.MyViewHolder>{
+
+        @Override
+        public ItemListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(getActivity()).inflate(R.layout.item_list_holder, parent, false));
+
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ItemListAdapter.MyViewHolder holder, int position) {
+            holder.tv.setText("hah");
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+
+
+        class MyViewHolder extends RecyclerView.ViewHolder{
+            TextView tv;
+            public MyViewHolder(View view){
+                super(view);
+                tv = (TextView)view.findViewById(R.id.tv_item);
+            }
+        }
     }
 }
