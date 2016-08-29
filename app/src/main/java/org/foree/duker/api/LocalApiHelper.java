bb9745b769ecf1a8024f82e84caf0fba1ed78685
foree <beijing.2008.lm@163.com>
2016-08-29 19:55:25 +0800

    category与subscription从文件中获取，待处理category与subscription多对多的映射在数据库中的保存之后考虑新的存储方式

--- app/src/main/java/org/foree/duker/api/LocalApiHelper.java
+++ app/src/main/java/org/foree/duker/api/LocalApiHelper.java
@@ -1,10 +1,5 @@
 package org.foree.duker.api;
 
-import android.util.Log;
-
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-
 import org.foree.duker.base.BaseApplication;
 import org.foree.duker.base.MyApplication;
 import org.foree.duker.dao.RssDao;
@@ -13,14 +8,11 @@ import org.foree.duker.rssinfo.RssCategory;
 import org.foree.duker.rssinfo.RssFeed;
 import org.foree.duker.rssinfo.RssItem;
 import org.foree.duker.rssinfo.RssProfile;
-import org.foree.duker.utils.FeedlyApiUtils;
 import org.foree.duker.utils.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by foree on 16-7-25.
@@ -33,14 +25,21 @@ public class LocalApiHelper extends FeedlyApiHelper {
     @Override
     public void getCategoriesList(String token, final NetCallback<List<RssCategory>> netCallback) {
 
-        RssDao rssDao = new RssDao(BaseApplication.getInstance().getApplicationContext());
-        List<RssCategory> rssCategories = rssDao.readCategory();
+        String localCategories = "";
+
+        final File category_json = new File(MyApplication.myApplicationDirPath + File.separator + MyApplication.myApplicationDataName + File.separator + "categories.json");
+
+        try {
+            localCategories = FileUtils.readFile(category_json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         if(netCallback != null){
-            if( !rssCategories.isEmpty())
-                netCallback.onSuccess(rssCategories);
+            if( !localCategories.isEmpty())
+                netCallback.onSuccess(parseCategories(localCategories));
             else
-                netCallback.onFail("rssCategories is null from db");
+                netCallback.onFail("rssCategories is empty");
         }
     }
 
@@ -61,8 +60,9 @@ public class LocalApiHelper extends FeedlyApiHelper {
 
     @Override
     public void getSubscriptions(String token, final NetCallback<List<RssFeed>> netCallback) {
-        token = API_TOKEN_TEST;
+
         String localSubscriptions = "";
+
         final File subscriptions_json = new File(MyApplication.myApplicationDirPath + File.separator + MyApplication.myApplicationDataName + File.separator + "subscriptions.json");
 
         try {
@@ -71,34 +71,12 @@ public class LocalApiHelper extends FeedlyApiHelper {
             e.printStackTrace();
         }
 
-        if (localSubscriptions.isEmpty()){
-        final Map<String,String> headers = new HashMap<>();
-        headers.put("Authorization","OAuth " + token);
-        NetWorkApiHelper.newInstance().getRequest(FeedlyApiUtils.getApiSubscriptionsUrl(), headers, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.i(TAG,"onResponse:getSubscriptions " + response);
-                try {
-                    FileUtils.writeFile(subscriptions_json, response);
-                    if ( netCallback != null){
-                        netCallback.onSuccess(parseSubscriptions(response));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        if (netCallback != null){
+            if( !localSubscriptions.isEmpty()){
+                netCallback.onSuccess(parseSubscriptions(localSubscriptions));
+            }else{
+                netCallback.onFail("localSubscription is empty");
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG,"onErrorResponse:getSubscriptions " + error.getMessage());
-
-                if (netCallback != null){
-                    netCallback.onFail(error.getMessage());
-                }
-            }
-        });
-        } else if ( netCallback != null){
-            netCallback.onSuccess(parseSubscriptions(localSubscriptions));
         }
     }
 
