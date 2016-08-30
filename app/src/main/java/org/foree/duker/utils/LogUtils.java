d5a13a73211c726a54743a8ccad08b3d6dca4065
foree <beijing.2008.lm@163.com>
2016-08-30 19:59:04 +0800

    添加日志的时间戳

--- app/src/main/java/org/foree/duker/utils/LogUtils.java
+++ app/src/main/java/org/foree/duker/utils/LogUtils.java
@@ -6,6 +6,7 @@ import org.foree.duker.base.MyApplication;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by foree on 16-8-21.
@@ -16,8 +17,8 @@ public class LogUtils {
 
     public static void log(String logMsg){
         File logFile = new File(MyApplication.myApplicationDirPath + File.separator + MyApplication.myApplicationCacheName + File.separator + "logfile");
-
-        FileUtils.appendFile(logFile, logMsg);
+        String currentTime = new Date(System.currentTimeMillis()).toString();
+        FileUtils.appendFile(logFile, currentTime + ":" + logMsg);
         Log.d(TAG, "write log to " + logFile.toString());
     }
 }
