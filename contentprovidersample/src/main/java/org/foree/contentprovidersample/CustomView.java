392fe34e84083c74fa3f12865d4f7e79cf139db3
foree <beijing.2008.lm@163.com>
2016-10-24 19:41:06 +0800

    增加自定义View的示例程序

---
+++ contentprovidersample/src/main/java/org/foree/contentprovidersample/CustomView.java
@@ -0,0 +1,16 @@
+package org.foree.contentprovidersample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by foree on 16-10-21.
+ */
+
+public class CustomView extends View {
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
